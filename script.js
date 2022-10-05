85c3eb660ae3c73cc9be83077614968321faecb4
juroga <juanjrgast@gmail.com>
2022-10-05 19:03:10 +0200

    Fixed the createSquares function by changing the quotes of "${}" by `${}`. Now working

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //Create a variable for the number of columns/lines. They will always
-//Mantain proportion so it will be the same
+//Maintain proportion so it will be the same
 let columnLineNumber = 16;
-let totalSquareNumber = columnLineNumber*columnLineNumber
+let totalSquareNumber = columnLineNumber*columnLineNumber;
+const squareWidth = 640/columnLineNumber;
 
 //Function that creates squares depending on the columnLineNumber
 //variable. It will stop when reached it.
@@ -13,6 +14,16 @@ function createSquares () {
         console.log(columnLineNumber);
         const square = document.createElement("div");
         square.classList.add("square");
+        square.style.margin = "0";
+        square.style.width = `${squareWidth}` + "px";
+        square.style.flexGrow = "1"; 
+        square.style.flexShrink = "1";
+        square.style.flexBasis = "initial";
         whiteboardContainer.appendChild(square);
     };
+}
+
+//Change the square background color to black when hovering over
+function blackSquare () {
+    square.style[background-color] = "black";
 }
