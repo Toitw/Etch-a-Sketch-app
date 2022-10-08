deb236198e9b2598f361d865b2b9e43506adc98a
Toitw <juanjrgast@gmail.com>
2022-10-08 11:32:19 +0200

    Added a line to start the createSquare function just after the body loads on the page

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+//Start the grid from the beginning 16x16
+let body = document.querySelector("body");
+body.onload = createSquares;
+
 //Create a variable for the number of columns/lines. They will always
-//Mantain proportion so it will be the same
+//Maintain proportion so it will be the same
 let columnLineNumber = 16;
-let totalSquareNumber = columnLineNumber*columnLineNumber
+let totalSquareNumber = columnLineNumber*columnLineNumber;
+const squareWidth = 640/columnLineNumber;
 
 //Function that creates squares depending on the columnLineNumber
 //variable. It will stop when reached it.
@@ -13,6 +18,17 @@ function createSquares () {
         console.log(columnLineNumber);
         const square = document.createElement("div");
         square.classList.add("square");
+        square.style.margin = "0";
+        square.style.width = `${squareWidth}` + "px";
+        square.style.flexGrow = "1"; 
+        square.style.flexShrink = "1";
+        square.style.flexBasis = "initial";
         whiteboardContainer.appendChild(square);
+        square.addEventListener("mouseover", blackSquare);
     };
+}
+
+//Change the square background color to black when hovering over
+function blackSquare () {
+    square.style[background-color] = "black";
 }
